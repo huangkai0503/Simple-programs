f78a04c562804b4466f0d1c5b2dcd055672626bb
黄恺 <75919906+huangkai0503@users.noreply.github.com>
2022-07-10 23:31:44 +0800

    Update hardware_sim_simpleS_t.py
    I forget to add SRLatch last time.

--- hardware-sim/hardware_sim_simpleS_t.py
+++ hardware-sim/hardware_sim_simpleS_t.py
@@ -47,54 +47,33 @@ class XorSizeTest(Testbench[XorSize]):
     pass
 
 
-def main():
-    from hardware_sim_simpleS import _gen_bin
-    # 初始化
-    inputs = ('a', size), ('b', size)
-    outputs = ('out', size),
-    kwargs: dict[str, Wire] = {}
-    for name, width in inputs + outputs:
-        kwargs[name] = Wire(name, width)
-    # 生成数据
-    pos = 0
-    name2pos = {}
-    for name, width in inputs:
-        name2pos[name] = pos, pos + width
-        pos += width
-    res: dict[str, list[str]] = {}
-    for data in _gen_bin(pos):
-        for name, pos in name2pos.items():
-            kwargs[name].write(data[pos[0]:pos[1]])
-        kwargs['out'].write(format(int(kwargs['a'].read().replace('x', '0').replace('z', '0'), 2)
-                                   ^ int(kwargs['b'].read().replace('x', '0').replace('z', '0'), 2), '0b'))
-        output = []
-        for name, _ in outputs:
-            output.append(kwargs[name].read())
-        res[data] = output
-    # 输出数据
-    names = [name for name, _ in inputs + outputs]
-    lines: list[list[str]] = []
-    for data, output in res.items():
-        line = []
-        for name, pos in name2pos.items():
-            line.append(data[pos[0]:pos[1]])
-        for o in output:
-            line.append(o.replace('x', '0').replace('z', '0'))
-        lines.append([str(i) for i in line])
-    most_width = 0  # 对齐
-    for lst in [names, *lines]:
-        for value in lst:
-            length = len(value)
-            if length > most_width:
-                most_width = length
-    str_res = []
-    for line in [names] + lines:
-        str_res.append('| ' + ' | '.join(f'{i:{most_width}}' for i in line) + ' |')
-        if line is names:  # 输出分隔线
-            str_res.append(('|' + '-' * (most_width + 2)) * 3 + '|')
-    print('\n'.join(str_res))
+class SRLatch(Module):
+    s_: Input
+    r_: Input
+    q: Output
+    q_: Output  # 结尾带 _ 表示反逻辑
+
+    def build(self):
+        s_, r_, q, q_ = self.s_, self.r_, self.q, self.q_
+        Nand(a=s_, b=self.q_, out=self.q)
+        Nand(a=r_, b=self.q, out=self.q_)
+
+
+class SRLatchTest(Testbench):
+    @classmethod
+    def run(cls):
+        s_ = Wire('s_')
+        r_ = Wire('r_')
+        q = Wire('q')
+        q_ = Wire('q_')
+        test = SRLatch(s_=s_, r_=r_, q=q, q_=q_)
+        print('s_, r_, q, q_ = ...')
+        for s_value, r_value in ('01', '11', '10', '11'):
+            s_.write(s_value)
+            r_.write(r_value)
+            run_until_stop(test)
+            print(s_.read(), r_.read(), q.read(), q_.read())
 
 
 if __name__ == '__main__':
-    XorSizeTest.run()
-    # main()
+    SRLatchTest.run()
